b8bb9509bc15be076c21c2f5c4e4886837b519d5
intTom <1065286464@qq.com>
2021-05-21 00:39:03 +0800

    '最近提交'

--- src/main/java/com/inttom/BlogApplication.java
+++ src/main/java/com/inttom/BlogApplication.java
@@ -1,12 +1,17 @@
 package com.inttom;
 
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApplication {
 
+    static Log log = LogFactory.get();
+
     public static void main(String[] args) {
+        log.info("start");
         SpringApplication.run(BlogApplication.class, args);
     }
 
