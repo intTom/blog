ab24b43daad71dd1e3d82eb58986bb69214ac68a
intTom <1065286464@qq.com>
2021-06-21 23:35:44 +0800

    '最近提交'

--- src/test/java/com/inttom/BlogApplicationTests.java
+++ src/test/java/com/inttom/BlogApplicationTests.java
@@ -8,6 +8,22 @@ class BlogApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println(ladder(11));
+    }
+
+
+    /**
+     * 走台阶，一次可以走一个台阶，或者 走两个台阶：斐波那契数列
+     */
+    @Test
+    int ladder(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 2;
+        } else {
+            return ladder(n - 1) + ladder(n - 2);
+        }
     }
 
 }
